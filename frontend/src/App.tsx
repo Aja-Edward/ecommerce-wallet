280befbd3cfc88870b4a4fe68a6ea001dbd8ff87
unknown <qualitycontents750@gmail.com>
2025-09-10 20:32:54 +0100

    Improved Privacy Section component so it shows on the browser

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './index.css';
 import BalanceCard from './components/BalanceCard';
 import TonyWalletBalance from './components/TonyWalletBalance';
 import Sample from './components/Sample';
+import PrivacySection from './components/PrivacySection';
 function App() {
   return (
     <div className="bg-gray-200 text-blue-700 p-4 rounded-lg shadow-lg flex flex-col items-center justify-center min-h-screen">
@@ -11,6 +12,9 @@ function App() {
       <BalanceCard title='Ecommerce wallet balance' balance={1000}/>
       <Sample description='This is a sample description for our Sample component.' />
       <TonyWalletBalance balance={5000}/>
+      <PrivacySection title='Privacy section' description='Manage your privacy settings and review your balance securely. This
+        section gives you an overview of your account and ensures your data
+        stays safe.' />
     </div>
   )
 }
--- frontend/src/components/PrivacySection.tsx
+++ frontend/src/components/PrivacySection.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import BalanceCard from "./BalanceCard";
 import Button from "./Button";
 
-const PrivacySection: React.FC = () => {
+interface PrivacySectionProps {
+  description: string;
+  title: string;
+}
+const PrivacySection: React.FC<PrivacySectionProps> = ({description, title}) => {
   return (
     <div className="bg-red-900 text-white p-4 m-2 rounded-lg shadow-md">
-      <h1>Privacy section</h1>
+      <h1>{title}</h1>
       <BalanceCard title="Different amount" balance={7700} />
       <p>
-        Manage your privacy settings and review your balance securely. This
-        section gives you an overview of your account and ensures your data
-        stays safe.
+        {description}
       </p>
       <Button cta="update me" />
     </div>
